dbe4fd88b251c72a1bf5558a5999a14ccf318016
14roebl <32593798+14roebl@users.noreply.github.com>
2018-06-26 13:33:15 +0200

    Update GraphAnalyzer_extended.py

--- GraphAnalyzer_extended.py
+++ GraphAnalyzer_extended.py
@@ -1,18 +1,22 @@
 from qgis.networkanalysis import *
 
+
+''' calculate neighborhood of a given vertex in a graph'''
 def neighborhood(graph, v_idx):
     N = []
     OutArcs = graph.vertex(v_idx).outArc()
     for j in OutArcs:
         N.append(graph.arc(j).inVertex())
     return N
-    
+
+''' calculate weight of an edge - give graph and two vertexes of edge'''
 def weight(graph, v_idx, neighbor):
     OutArcs = graph.vertex(v_idx).outArc()
     for j in OutArcs:
         if graph.arc(j).inVertex() == neighbor:
             return graph.arc(j).property(0)
-            
+
+''' find edge of a graph given two vertexes'''
 def findEdge(graph, v_idx, neighbor):
     OutArcs = graph.vertex(v_idx).outArc()
     for j in OutArcs:
